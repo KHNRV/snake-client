96dfca4af64ea1a786bd1373db2f1e1447701247
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-10 17:36:47 +0000

    Added basic controls

--- input.js
+++ input.js
@@ -1,18 +1,33 @@
+// Stores the active TCP connection object.
+let connection;
+
 const handleUserInput = (key) => {
   if (key === "\u0003") {
     // at which point the program should say "Thanks for using me, ciao!"
     console.log("Thanks for using me, ciao!");
     // before terminating
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+    // console.log("up");
+  } else if (key === "s") {
+    connection.write("Move: down");
+    // console.log("down");
+  } else if (key === "a") {
+    connection.write("Move: left");
+    // console.log("left");
+  } else if (key === "d") {
+    connection.write("Move: right");
+    // console.log("right");
   }
-  console.log(key);
 };
 
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   // Allows to read the CLI
   const readline = require("readline");
